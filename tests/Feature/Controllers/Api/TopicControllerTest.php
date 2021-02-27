7d8a9326dbdfc9dfa91a4db7a3c55da8c1485dfa
fantasy0107 <fantasyg0107@gmail.com>
2021-02-27 10:10:53 +0800

    test TopicController and TargetController

--- tests/Feature/Controllers/Api/TopicControllerTest.php
+++ tests/Feature/Controllers/Api/TopicControllerTest.php
@@ -2,62 +2,51 @@
 
 namespace Tests\Feature\Controllers\Api;
 
-
-
+use App\Models\User;
+use App\Models\UserToken;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\User;
-use App\Models\UserToken;
 
-class TargetControllerTest extends TestCase
+class TopicControllerTest extends TestCase
 {
-    
-
     private $bearerToken = 'Bearer eyJpdiI6Ii9jRk9YeWw1K0VXakRSSlRBMTBtbEE9PSIsInZhbHVlIjoieUtIaldQbTBjc3ZWNG44UnJLSkxFUT09IiwibWFjIjoiNmIyYWQ2OTJkNGI0OWEwNmEwNWJkOGVmNDQ5ZTdjNGM5NWQwNzA0YTc0ZjliYzNlNmY0ZmU3Y2JhOTU2YTMxYiJ9';
 
     private $token = 'eyJpdiI6Ii9jRk9YeWw1K0VXakRSSlRBMTBtbEE9PSIsInZhbHVlIjoieUtIaldQbTBjc3ZWNG44UnJLSkxFUT09IiwibWFjIjoiNmIyYWQ2OTJkNGI0OWEwNmEwNWJkOGVmNDQ5ZTdjNGM5NWQwNzA0YTc0ZjliYzNlNmY0ZmU3Y2JhOTU2YTMxYiJ9';
 
-
     public function test_index()
     {
         $this->withHeaders([
-            'Authorization' =>  $this->bearerToken
-        ])->json('get', 'api/targets')->assertStatus(200);
+            'Authorization' => $this->bearerToken
+        ])->json('get', 'api/topics')->assertStatus(200);
     }
 
     public function test_store()
     {
         $faker = \Faker\Factory::create();
 
+        $title = $faker->name;
         $this->withHeaders([
-            'Authorization' =>  $this->bearerToken
-        ])->json('post', 'api/targets', [
-            'name' =>  $faker->name
-        ])->assertStatus(201);
-    }
-
-    public function test_store_fail_for_no_name()
-    {
-        $this->withHeaders([
-            'Authorization' =>  $this->bearerToken
-        ])->json('post', 'api/targets')->assertStatus(422);
+            'Authorization' => $this->bearerToken
+        ])->json('post', 'api/topics', [
+            'title' => $title,
+            'content' => $faker->paragraph
+        ])->assertStatus(201)->assertSeeText($title);
     }
 
     public function test_update()
     {
-        $faker = \Faker\Factory::create();
         $userToken = UserToken::where('value', $this->token)->first();
         $user = User::find($userToken->user_id);
 
-        $target = $user->targets()->first();
+        $target = $user->topics()->first();
+        $faker = \Faker\Factory::create();
 
-        $name = $faker->name;
-
+        $title = $faker->name;
         $this->withHeaders([
-            'Authorization' =>  $this->bearerToken
-        ])->patch('api/targets/' . $target->id, [
-            'name' => $name
-        ])->assertStatus(200)->assertSeeText($name);
+            'Authorization' => $this->bearerToken
+        ])->json('patch', 'api/topics/' . $target->id, [
+            'title' => $title
+        ])->assertStatus(200)->assertSeeText($title);
     }
 }
